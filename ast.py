c4b4869037d6e2c1ca6f98e7963ca97a95491b73
Matte <mattias4@kth.se>
2018-03-03 15:02:38 +0100

    Added more AST classes.

--- ast.py
+++ ast.py
@@ -27,3 +27,60 @@ class BinopAexp(Aexp):
 
     def __repr__(self):
         return f'BinopAexp({self.op}, {self.left}, {self.right})'
+
+
+class Bexp(Equality):
+    pass
+
+
+class RelopBexp(Bexp):
+    def __init__(self, op, left, right):
+        self.op = op
+        self.left = left
+        self.right = right
+
+
+class AndBexp(Bexp):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class OrBexp(Bexp):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class NotBexp(Bexp):
+    def __init__(self, exp):
+        self.exp = exp
+
+
+class Statement(Equality):
+    pass
+
+
+class AssignStatement(Statement):
+    def __init__(self, name, aexp):
+        self.name = name
+        self.aexp = aexp
+
+
+class CompoundStatement(Statement):
+    def __init__(self, first, second):
+        self.second = second
+        self.first = first
+
+
+class IfStatement(Statement):
+    def __init__(self, condition, true_stmt, false_stmt):
+        self.condition = condition
+        self.true_stmt = true_stmt
+        self.false_stmt = false_stmt
+
+
+class WhileStatement(Statement):
+    def __init__(self, condition, body):
+        self.condition = condition
+        self.body = body
